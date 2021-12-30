f12b8fccd2ce7068c23d27e6b1894185a05e072c
CSIE <CSIE@DESKTOP-P8EL5IA>
2021-12-30 17:56:03 +0800

    edf1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Window1.xaml 的互動邏輯
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Color currentStrokeColor = Colors.Black;
+        Brush currentStrokeBrush = new SolidColorBrush(Colors.PaleGreen);
+        Brush currentStrokeBrush1 = new SolidColorBrush(Colors.Pink);
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+          
+            textBlock.Text = label + "分數:" +label1+ label2 + "分數:" +label3+ label4 + "分數:"+label5;
+            if (slider.Value + slider1.Value + slider2.Value > 60)
+            {
+                textBlock.Background = currentStrokeBrush;
+            }
+            else
+                textBlock.Background= currentStrokeBrush1;
+
+
+        }
+
+        
+    }
+}
